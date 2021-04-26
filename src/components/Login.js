7d17a167021f758997ccc832eacf4e7e4a2d0cf6
akashghanate <akashghanate98@gmail.com>
2021-04-26 21:49:00 +0530

    adding home anchor to nav bar

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,16 @@ const Login = (props) => {
     return(
         <Container>
             <Content>
+                <CTA>
+                    <CTALogoOne src="/images/cta-logo-one.svg" alt="Hi"/>
+                    <SignUp>Get All There</SignUp>
+                    <Description>
+                        Get Premier Access to Raya and the Last Dragon for
+                        an additional fee with Disney+ subscription. As of 26/04/21, the price of Disney+ and
+                        The Disney Bundle will increase by Rs.100.
+                    </Description>
+                    <CTALogoTwo src="/images/cta-logo-two.png" alt='' />
+                </CTA>
                 <BgImage />
             </Content>
         </Container>
@@ -39,6 +49,65 @@ const BgImage = styled.div`
     position: absolute;
     background-size: cover;
     background-image: url("images/login-background.jpg");
+`;
+
+const CTA = styled.div`
+    margin-bottom: 2vw;
+    max-width: 650px;
+    flex-wrap: wrap;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 0;
+    align-items: center;
+    text-align: center;
+    margin-right: auto;
+    margin-left: auto;
+    transition-timing-function: ease-out;
+    transition: opacity 0.2s;
+    width: 100%;
+`;
+
+const CTALogoOne = styled.img`
+    margin-bottom: 12px;
+    max-width: 600px;
+    min-height: 1px;
+    display: block;
+    width: 100%;
+`;
+
+const SignUp = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    width: 100%;
+    margin-bottom: 12px;
+    letter-spacing: 1.5px;
+    text-transform: uppercase;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent; 
+    border-radius: 4px;
+
+    &:hover {
+        background-color: #0483ee;
+    }
+`;
+
+const Description = styled.p`
+    color: hsla(0, 0%, 95.3%, 1);
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+    margin-bottom: 20px;
+    max-width: 600px;
+    width: 100%;
+    display: inline-block;
+    vertical-align: bottom;
 `;
 
 export default Login;
