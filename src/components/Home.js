84be8674ffd673560b392dc6c09aac516c5ba961
akashghanate <akashghanate98@gmail.com>
2021-04-28 19:13:49 +0530

    added bgImage for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,35 @@
 import styled from 'styled-components';
+import config from '../config';
 
+const bgImg = config.baseURl+'/images/home-background.png';
 const Home = (props) => {
     return(
-        <div>In Home</div>
+        <Container>
+            <h2>Home</h2>
+            <h2>Home</h2>
+            <h2>Home</h2>
+            <h2>Home</h2>
+            <h2>Home</h2>
+        </Container>
     )
 };
+
+const Container = styled.main`
+    position: relative;
+    min-height: calc(100vh - 250px);
+    overflow-x: hidden;
+    display: block;
+    top: 72px;
+    padding: 0 calc(3.5vw + 5px);
+    
+    &:after{
+        background: url(${bgImg}) center center / cover no-repeat fixed;
+        content: '';
+        position: absolute; 
+        inset: 0px;
+        opacity: 1;
+        z-index: -1;
+    }
+`;
 
 export default Home;
